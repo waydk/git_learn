91e2bcd35b6b82d21c6f87c0db4cfe25d624405b
Vikushka <vikushka@MacBook-Air-Vikushka.local>
2026-06-08 09:15:25 +0300

    new file ff.js

---
+++ ff.js
@@ -0,0 +1,14 @@
+function generatePassword(length = 12) {
+    const chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        password += chars[Math.floor(Math.random() * chars.length)];
+    }
+
+    return password;
+}
+
+console.log(generatePassword(16));
